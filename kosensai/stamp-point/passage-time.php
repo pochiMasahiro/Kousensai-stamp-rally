940cf2a9181776c4a32576bb920165b4b58e9c0d
pochiMasahiro <pochi.masahiro@gmail.com>
2015-10-04 20:52:25 +0900

    bug fix

--- kosensai/stamp-point/passage-time.php
+++ kosensai/stamp-point/passage-time.php
@@ -2,10 +2,27 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<title>通過状況</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- My CSS -->
+		<link href="section.css" rel="stylesheet">
+	
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
 	<body>
-		<table>
+		<div class="container">
+		<table class="table">
 			<tr>
 				<th>ナンバー</th>
 				<th>場所</th>
@@ -28,6 +45,7 @@
 				$smtp = $pdo -> prepare('INSERT INTO PASSAGE_TIME (IDM, POINT) VALUES(:IDM, :POINT)');
 				$smtp -> bindValue(':IDM', $idm);
 				$smtp -> bindValue(':POINT', $point);
+				$smtp -> execute();
 			}
 			
 			$get_passage = $pdo -> prepare('SELECT check_point.point, check_point.name, passage_time.time FROM check_point LEFT JOIN passage_time ON check_point.point = passage_time.point and passage_time.idm = :idm order by check_point.point asc');
@@ -52,5 +70,6 @@
 			</tr>
 			<?php endwhile; ?>
 		</table>
+		</div>
 	</body>
 </html>
